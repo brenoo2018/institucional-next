8233a0011641c4a70507c4fa85c2b986cda4117a
Taynan Breno <thaynanbreno@gmail.com>
2024-06-18 14:25:59 -0300

    feat: add request to blog

--- src/app/lib/api.ts
+++ src/app/lib/api.ts
@@ -1,19 +1,3 @@
-export async function fetchAPI(
-  endpoint: string,
-  params: string = ''
-): Promise<any> {
-  const url = `${process.env.WORDPRESS_API_URL}${endpoint}${params}`;
-  const res = await fetch(url);
-  const data = await res.json();
-
-  if (!res.ok) {
-    console.error(data);
-    throw new Error('Failed to fetch API');
-  }
-
-  return data;
-}
-
 export async function getPosts(
   page: number = 1,
   perPage: number = 20
@@ -29,25 +13,52 @@ export async function getPosts(
   }
 
   const total = parseInt(res.headers.get('X-WP-Total') || '0', 10);
+
   return { posts: data, total };
 }
 
 export async function getPostBySlug(slug: string): Promise<any> {
   const params = `?slug=${slug}`;
-  const posts = await fetchAPI('posts', params);
-  return posts[0];
+  const url = `${process.env.WORDPRESS_API_URL}/posts${params}`;
+  const res = await fetch(url);
+  const data = await res.json();
+
+  if (!res.ok) {
+    console.error(data);
+    throw new Error('Failed to fetch API');
+  }
+
+  return { post: data[0] };
 }
 
 export async function getPages(
   page: number = 1,
   perPage: number = 20
-): Promise<any[]> {
+): Promise<{ pages: any[]; total: number }> {
   const params = `?page=${page}&per_page=${perPage}`;
-  return fetchAPI('pages', params);
+
+  const url = `${process.env.WORDPRESS_API_URL}/pages${params}`;
+  const res = await fetch(url);
+  const data = await res.json();
+
+  if (!res.ok) {
+    console.error(data);
+    throw new Error('Failed to fetch API');
+  }
+
+  const total = parseInt(res.headers.get('X-WP-Total') || '0', 10);
+  return { pages: data, total };
 }
 
 export async function getPageBySlug(slug: string): Promise<any> {
   const params = `?slug=${slug}`;
-  const pages = await fetchAPI('pages', params);
-  return pages[0];
+  const url = `${process.env.WORDPRESS_API_URL}/pages${params}`;
+  const res = await fetch(url);
+  const data = await res.json();
+
+  if (!res.ok) {
+    console.error(data);
+    throw new Error('Failed to fetch API');
+  }
+  return { page: data[0] };
 }
