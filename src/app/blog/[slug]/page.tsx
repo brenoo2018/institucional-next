c5f6c7fc110fff1cede390e732ebc7c68fc0f768
Taynan Breno <thaynanbreno@gmail.com>
2024-06-18 12:03:51 -0300

    config metadata

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,6 +1,26 @@
+import { Metadata } from 'next';
+
 type TPropsPost = {
   params: {
     slug: string;
+  };
+};
+
+// export const generateMetadata = ({ params }: TPropsPost): Metadata => {
+//   return {
+//     title: `Post ${params.slug}`,
+//     description: 'Here you can find my blog posts.',
+//   };
+// };
+export const generateMetadata = async ({
+  params,
+}: TPropsPost): Promise<Metadata> => {
+  const title = await new Promise((resolve) => setTimeout(resolve, 1000)).then(
+    () => `Post ASYNC ${params.slug}`
+  );
+  return {
+    title,
+    description: 'Here you can find my blog posts.',
   };
 };
 
